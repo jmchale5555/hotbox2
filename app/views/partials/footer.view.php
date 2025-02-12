ca1e0401779d5fc7e0004851bec3e1c8b2bf44ae
root <root@kuyt.ad.ee.ucl.ac.uk>
2025-02-12 16:05:21 +0000

    Add existing project files to Git

---
+++ app/views/partials/footer.view.php
@@ -0,0 +1,4 @@
+<script defer src='./dist/app.js'></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script> -->
+</div>
+</body>
---
+++ app/views/partials/header.view.php
@@ -0,0 +1,10 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="<?= ROOT ?>/assets/css/output.css" rel="stylesheet">
+    <?php include 'navbar.view.php' ?>
+</head>
+
+<body>
+
+    <div class="max-w-screen-lg w-4/5 mx-auto p-4 h-screen">
