2ced8e459a72fdaaf2fb45404ff5883621e68726
James <jmchale5555@gmail.com>
2025-04-13 14:30:02 +0100

    build image upload 002

--- app/views/admin.view.php
+++ app/views/admin.view.php
@@ -70,8 +70,12 @@
                           Number of Desks
                       </th>
                       <th scope="col" class="col-span-2 px-6 py-3">
+                          Room Plan
+                      </th>
+                      <th scope="col" class="col-span-2 px-6 py-3">
                           Actions
                       </th>
+
                       <th scope="col" class="col-span-2 px-6 py-3">
  
                     <button type="button" @click="newRoom(rooms[0])" class="float-right min-w-max text-white !bg-gradient-to-br !from-purple-600 !to-blue-500 !hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2 text-center my-3 me-2 mb-2">
